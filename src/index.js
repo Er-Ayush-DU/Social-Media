087eb409a14ab71ad376a0f8942edc075c586554
Er-Ayush-DU <er.ayush336@gmail.com>
2025-01-16 01:41:21 +0530

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import dotenv from "dotenv";
+import express from "express"
 import connectDB from "./db/db.js";
 
 dotenv.config({
   path: "./env"
 });
+
+const app = express(); // Define the app instance
 
 connectDB()
 
